f3dcf06c4ba44d95f339cb731c5f5c7976e5cd36
sheheem <sheheemzainudeen@gmail.com>
2023-01-17 12:13:49 +0530

    edited banner, row poster

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule} from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
+import { SearchComponent } from './pages/search/search.component';
+import { MovieDetailsComponent } from './pages/movie-details/movie-details.component';
+import { MovieApiServiceService } from './service/movie-api-service.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    SearchComponent,
+    MovieDetailsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [MovieApiServiceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MovieApiServiceService } from 'src/app/service/movie-api-service.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
+  constructor(private service: MovieApiServiceService) {}
+
+  bannerResult: any = [];
+  trendingMovieResult: any = [];
+
+  ngOnInit(): void {
+      this.bannerData();
+      this.trendinData();
+  }
+
+  bannerData(){
+    this.service.bannerApiData().subscribe((result) => {
+      console.log(result, 'bannerresult#');
+      this.bannerResult = result.results;
+    })
+  }
+
+  trendinData() {
+    this.service.trendingMoiveApiData().subscribe((result) => {
+      console.log(result,'trendingresult#');
+      this.trendingMovieResult = result.results;
+      
+
+
+    })
+  }
 }
